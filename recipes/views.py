c3a71db55ee6743fc67e9e96dbd093142d8a2d42
Marceli Fioravante <marceli.fioravante@gmail.com>
2022-09-01 16:47:26 -0400

    Início do html da página home

--- recipes/views.py
+++ recipes/views.py
@@ -6,6 +6,6 @@ from django.shortcuts import render
 
 def home(request):
     # return HttpResponse('HOME')
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Marceli',
     })
